ce0b8f61a156befb8d8d0416fa6e85a3e99b2d1f
Alexander Sosa <alexandersosa2204@gmail.com>
2015-01-18 11:13:58 -0600

    First commit

---
+++ app/src/main/java/net/androidsensei/pokedex/api/PokedexApi.java
@@ -0,0 +1,32 @@
+package net.androidsensei.pokedex.api;
+
+import android.view.View;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import net.androidsensei.pokedex.PokedexApplication;
+import net.androidsensei.pokedex.model.Pokemon;
+import net.androidsensei.pokedex.model.PokemonAdapter;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by alex on 1/18/15.
+ */
+public class PokedexApi {
+    private static final String MI_POKEDEX_URL = "http://mi-pokedex.herokuapp.com/api";
+    private PokemonAdapter pokemonAdapter;
+    private ProgressBar progressBarLoading;
+    private ListView listView;
+    public static void getPokemons( Response.Listener listener,  Response.ErrorListener errorListener){
+         GsonRequest<Pokemon[]> getPersons =
+                new GsonRequest<Pokemon[]>(MI_POKEDEX_URL+"/pokemons", Pokemon[].class,
+
+                       listener, errorListener);
+        PokedexApplication.getInstance().addToRequestQueue(getPersons);
+    }
+}
